c54f5ebb606b0a00ecda6594349fe2e59b1d1001
Paul Reitz <paul.reitz@hbo.com>
2017-11-10 11:34:25 -0800

    typeo in jumpbox vars

--- jumpbox/vars.tf
+++ jumpbox/vars.tf
@@ -1,5 +1,5 @@
 variable "tfstate_bucket_name" {}
 variable "aws_region" {}
 variable "vpc_state_key" {}
-variable "security_group_state_key" {}
+variable "security_groups_state_key" {}
 variable "aws_key_pair_name" {}
